668505d01c7b2ba16612dbc5571ab9c36e82a01e
CarlosMedicsen <carlos.fernandez.medicsen@gmail.com>
2025-06-30 13:05:47 +0200

    basic power feedback implementation

--- src/PowerFeedback.py
+++ src/PowerFeedback.py
@@ -41,26 +41,25 @@ def PWfdbk():
     for i in np.linspace(0, 10, 100):  # Simulación por 10 segundos
         pass
 
-
 def SimpleFeedback() -> None:
     # Configuración del osciloscopio
-    osci = Osciloscopio("USB0::0xF4EC::0xEE3A::SDS1MKGX802538::0::INSTR", 2) #osciloscopio Siglent
-    print(osci)
+    osci = Osciloscopio("USB0::0xF4EC::0xEE3A::SDS1MKGX802538::INSTR", 2)
 
     # Configuración del generador de pulsos
-    pulser = Pulser("USB0::0xF4EC::0xEE38::SDG1XDDC801175::0::INSTR", 2)
-    pulser.set_frecuencia(38000)  # Frecuencia en Hz
+    pulser = Pulser("USB0::0xF4EC::0x1103::SDG1PA0C900147::INSTR", 2)
+    frecuencia = 38000
+    pulser.set_frecuencia(frecuencia)  # Frecuencia en Hz
     pulser.set_amplitud(0.25)  # Amplitud inicial en V
     print(pulser)
 
     # Configuración del PID
-    kP = 0.1 #Si hay 1W de diferenciam, el PID subirá 0.1V
-    kI = 0.01
-    kD = 0
+    kP = 1 #Si hay 1W de diferenciam, el PID subirá 0.1V
+    kI = 0.2
+    kD = 0.2
 
-    PowerRef = 8 #W
+    PowerRef = 2 #W
 
-    pid = PID(kP=kP, kI=kI, kD=kD, setpoint=PowerRef, max_output=0.8)
+    pid = PID(Kp=kP, Ki=kI, Kd=kD, setpoint=PowerRef)
     
     print("Iniciando el control de potencia...\n")
     print(f"Referencia de potencia: {PowerRef} W\n")
@@ -69,36 +68,40 @@ def SimpleFeedback() -> None:
 
     data = pd.DataFrame(columns=['Tiempo (s)', 'Potencia (W)', 'Salida PID (V)'])
     output = 0.0
-    pulser.output_on() 
-    t0 = time.time()
-    Texp = t0 + 10 
-    while time.time() < Texp:
-        P =  osci.MedirPotencia(1, 2)
-        dt = time.time() - t0
-        output = pid.update(P, dt=dt)
+    with pulser as pulser:
+        pulser.output_on()
         t0 = time.time()
-        pulser.set_amplitud(pulser.amp1 + output)
-        data = data.append({'Tiempo (s)': dt, 'Potencia (W)': P, 'Salida PID (V)': output}, ignore_index=True)
-    pulser.output_off()
+        Texp = t0 + 10
+        while time.time() < Texp:
+            try:
+                pulser.set_amplitud(pulser.amp1 + output)
+            except ValueError:
+                pulser.set_amplitud(0.6)
+            P = osci.MedirPotencia(1, 2)
+            t = time.time()
+            dt = t - t0
+            output = pid.update(P, dt=dt)
+            t0 = time.time()
+            #print(f"PID output: {output:.2f} V, Potencia medida: {P:.2f} W, Tiempo: {time.time():.2f} s")
+            #print(f"seting amplitude to {pulser.amp1 + output:.2f} V")
+            nueva_fila = pd.DataFrame([{'Tiempo (s)': t, 'Potencia (W)': P, 'Salida PID (V)': output}])
+            data = pd.concat([data, nueva_fila], ignore_index=True)
+        pulser.output_off()
 
     print("Control de potencia finalizado.\n")
     
+    print(data)
+
     # Graficar resultados
     plt.figure(figsize=(10, 6))
     plt.plot(data['Tiempo (s)'], data['Potencia (W)'], label='Potencia medida (W)', color='blue')
     plt.plot(data['Tiempo (s)'], data['Salida PID (V)'], label='Salida PID (V)', color='orange')
     plt.xlabel('Tiempo (s)')
     plt.ylabel('Valor')
-    plt.title('Control de Potencia con PID')
+    plt.title(f"Control de Potencia con PID a f = {frecuencia}")
     plt.legend()
     plt.grid()
     plt.show()
-
-    
-
-
-
-
 
 def stepResponse(osci: Osciloscopio, pulser: Pulser, pid: PID) -> None:
     """
@@ -144,5 +147,5 @@ def bucleMedicion(osci: Osciloscopio, potencia_queue: queue.Queue, tiempoSampleo
         time.sleep(tiempoSampleo)  # Espera [segundos] entre mediciones
 
 if __name__ == "__main__":
-    PWfdbk()
+    SimpleFeedback()
     
